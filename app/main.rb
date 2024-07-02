6966d981aff1b72c8cad9b8a16d06534040929e6
binarygit <inscription4@outlook.com>
2024-07-02 11:24:49 +0545

    Rewrite in OOP fashion

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,38 @@
-def tick args
-  args.state.rotation ||= 0
-  args.state.x ||= 576
-  args.state.y ||= 0
+class MyGame
+  attr_gtk
 
-  if args.inputs.mouse.click
-    args.state.x = args.inputs.mouse.click.point.x - 64
-    args.state.y = args.inputs.mouse.click.point.y - 50
+  def tick
+    defaults
+    calc
+    render
   end
 
-  args.outputs.labels << [580, 400, 'Hello World!']
-  args.outputs.sprites << [args.state.x, args.state.y, 128, 101, 'dragonruby.png', args.state.rotation]
-  args.state.rotation -= 1
+  private
+
+  def defaults
+    state.rotation ||= 0
+    state.x ||= 576
+    state.y ||= 0
+  end
+
+  def calc
+    if inputs.mouse.click
+      state.x = inputs.mouse.click.point.x - 64
+      state.y = inputs.mouse.click.point.y - 50
+    end
+  end
+
+  def render
+    args.outputs.labels << [580, 400, 'Hello World!']
+    args.outputs.sprites << [args.state.x, args.state.y, 128, 101, 'dragonruby.png', args.state.rotation]
+    args.state.rotation -= 1
+  end
 end
+
+def tick args
+  $game ||= MyGame.new
+  $game.args = args
+  $game.tick
+end
+
+$game = nil
